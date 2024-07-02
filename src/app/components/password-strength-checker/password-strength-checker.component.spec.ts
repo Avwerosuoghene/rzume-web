140120d2c79312bece98b7f1e52cae17dd924f31
Avwerosuoghene_Darhare-Igben <kesuion1@gmail.com>
2024-07-02 06:52:58 +0100

    feat(password-validator): adds unit test
    adds unit test for password strength check and for empty password

--- src/app/components/password-strength-checker/password-strength-checker.component.spec.ts
+++ src/app/components/password-strength-checker/password-strength-checker.component.spec.ts
@@ -1,8 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PasswordStrengthCheckerComponent } from './password-strength-checker.component';
+import { PasswordCriteria } from '../../core/helpers/constants';
 
-describe('PasswordStrengthCheckerComponent', () => {
+fdescribe('PasswordStrengthCheckerComponent', () => {
   let component: PasswordStrengthCheckerComponent;
   let fixture: ComponentFixture<PasswordStrengthCheckerComponent>;
 
@@ -19,5 +20,34 @@ describe('PasswordStrengthCheckerComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set password strength value and description correctly', () => {
+    const testCases = [
+      { password: 'short', expectedStrength: 1, expectedDescription: 'Weak' }, 
+      { password: 'longenough', expectedStrength: 2, expectedDescription: 'Medium' }, 
+      { password: 'Med1umPass', expectedStrength: 2, expectedDescription: 'Strong' }, 
+      { password: 'Str0ngPass!', expectedStrength: 4, expectedDescription: 'Strong' },
+    ];
+
+
+    spyOn(component.passwordStrengthValue, 'set').and.callThrough();
+
+
+    testCases.forEach(testCase => {
+      component.checkPasswordStrength(testCase.password);
+
+      expect(component.passwordStrengthValue.set).toHaveBeenCalledWith(testCase.expectedStrength);
+      expect(component.passwordStengthDescription).toEqual(testCase.expectedDescription);
+    });; // Assuming criteriaCount sets it to 3rd level
+  });
+
+  it('should handle empty password correctly', () => {
+    spyOn(component.passwordStrengthValue, 'set').and.callThrough();
+
+    component.checkPasswordStrength('');
+
+    expect(component.passwordStengthDescription).toEqual('nan');
+    expect(component.passwordStrengthValue.set).toHaveBeenCalledWith(0);
   });
 });
