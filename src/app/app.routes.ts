4fc9e992db185a5d593b7604ec42e12a205edca4
Avwerosuoghene_Darhare-Igben <kesuion1@gmail.com>
2024-07-17 20:34:51 +0100

    fixes error with / in rootRoutes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,12 @@ import { MainComponent } from './pages/main/main.component';
 import {  RootRoutes } from './core/models/enums/application-routes-enums';
 
 export const routes: Routes = [
-  { path: '', redirectTo: `/${RootRoutes.auth}`, pathMatch: "full" },
+  { path: '', redirectTo: `${RootRoutes.auth}`, pathMatch: "full" },
   {
 
-    path: `/${RootRoutes.auth}`, component: AuthenticationComponent, children: authenticationRoutes,
+    path: `${RootRoutes.auth}`, component: AuthenticationComponent, children: authenticationRoutes,
 
 
   },
-  { path: `/${RootRoutes.main}`, component: MainComponent }
+  { path: `${RootRoutes.main}`, component: MainComponent }
 ];
