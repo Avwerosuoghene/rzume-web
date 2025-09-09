187eb1e9aef26fd1b11c76bf85cc3b7355804459
Avwerosuoghene_Darhare-Igben <kesuion1@gmail.com>
2025-09-09 22:20:31 +0100

    create the jobCardList component

---
+++ src/app/components/job-card-list/job-card-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-job-card-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './job-card-list.component.html',
+  styleUrls: ['./job-card-list.component.scss']
+})
+export class JobCardListComponent {
+
+}
