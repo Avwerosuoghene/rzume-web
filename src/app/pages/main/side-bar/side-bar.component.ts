82798065a71769b06e77a017b65e4bada8200bf0
Avwerosuoghene_Darhare-Igben <kesuion1@gmail.com>
2025-09-09 08:41:03 +0100

    adds sidebar close button

--- src/app/pages/main/side-bar/side-bar.component.ts
+++ src/app/pages/main/side-bar/side-bar.component.ts
@@ -1,20 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { RouterModules } from '../../../core/modules/router-modules';
 import { MainRoutes, RootRoutes } from '../../../core/models/enums/application.routes.enums';
 import { MainComponent } from '../main.component';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-side-bar',
   standalone: true,
-  imports: [RouterModules],
+  imports: [CommonModule, RouterModules, MatIconModule],
   templateUrl: './side-bar.component.html',
   styleUrl: './side-bar.component.scss'
 })
 export class SideBarComponent {
+  @Output() closeSidebar = new EventEmitter<void>();
   sideBarElements!: Array< {name: string, icon: string, route: string}>;
 
   ngOnInit(): void {
     this.initializeSideBarNavs();
+  }
+
+  onClose(): void {
+    this.closeSidebar.emit();
   }
 
   initializeSideBarNavs(): void {
