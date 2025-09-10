0af99a8d521ab9d04abd2dfe39973c52cda9844c
Avwerosuoghene_Darhare-Igben <kesuion1@gmail.com>
2025-09-10 22:04:42 +0100

    includes add button on card-list

--- src/app/components/job-card-list/job-card-list.component.ts
+++ src/app/components/job-card-list/job-card-list.component.ts
@@ -4,11 +4,12 @@ import { JobCardTabsComponent } from './partials/job-card-tabs/job-card-tabs.com
 import { JobCardItemComponent } from './partials/job-card-item/job-card-item.component';
 import { JobApplicationItem } from '../../core/models/interface/job-application.models';
 import { JOB_FILTER_OPTIONS } from '../../core/models/constants/dashboard.constants';
+import { AngularMaterialModules } from '../../core/modules';
 
 @Component({
   selector: 'app-job-card-list',
   standalone: true,
-  imports: [CommonModule, JobCardTabsComponent, JobCardItemComponent],
+  imports: [CommonModule, JobCardTabsComponent, JobCardItemComponent, AngularMaterialModules],
   templateUrl: './job-card-list.component.html',
   styleUrls: ['./job-card-list.component.scss']
 })
@@ -16,6 +17,7 @@ export class JobCardListComponent implements OnChanges {
   @Output() jobApplicationUpdate = new EventEmitter<JobApplicationItem>();
   @Output() jobStatusUpdate = new EventEmitter<{ item: JobApplicationItem }>();
   @Output() jobApplicationsDelete = new EventEmitter<string[]>();
+  @Output() jobApplicationAddition = new EventEmitter<void>();
   @Input() jobs: JobApplicationItem[] = [];
   
   tabs = JOB_FILTER_OPTIONS.filter(option => option.value !== '' );
