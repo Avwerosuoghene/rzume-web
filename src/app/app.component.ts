e2e55fe8b3dc8ee0f7c24ec9ab7c7788d26b564a
Avwerosuoghene_Darhare-Igben <kesuion1@gmail.com>
2026-01-10 23:17:44 +0100

    adds glocal loader on app compoent load

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,18 @@
 import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
 import { RouterModules } from './core/modules/router-modules';
+import { LoaderService } from './core/services/loader.service';
+import { GlobalCircularLoaderComponent } from './components/global-circular-loader/global-circular-loader.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModules],
+  imports: [RouterModules, AsyncPipe, GlobalCircularLoaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
 
-  checkUserSession() {
-
-  }
-
+  constructor(public loaderService: LoaderService) {}
 
 }
