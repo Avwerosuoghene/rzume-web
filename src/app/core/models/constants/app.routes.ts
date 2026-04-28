361b8d1f309dff3af1b86559317a2bc6cd58b897
Avwerosuoghene_Darhare-Igben <kesuion1@gmail.com>
2026-04-28 20:53:07 +0100

    fix(authentication): fixes issue affecting auth guard

--- src/app/core/models/constants/app.routes.ts
+++ src/app/core/models/constants/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { RootRoutes } from '../enums/application.routes.enums';
+import { AuthGuardService } from '../../guards/auth.guard.service';
 
 export const routes: Routes = [
   { path: '', redirectTo: `${RootRoutes.auth}`, pathMatch: "full" },
@@ -16,7 +17,7 @@ export const routes: Routes = [
       .then(m => m.MainComponent),
     loadChildren: () => import('../../../pages/main/main-routes')
       .then(m => m.mainSectionRoutes),
-    canActivate: [() => import('../../guards/auth.guard.service').then(m => m.AuthGuardService)],
+    canActivate: [AuthGuardService],
     data: { preload: true } // Preload main routes after login
   },
   {
