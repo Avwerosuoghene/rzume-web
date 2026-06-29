b446ba2ff7fe22fc33b5a0a07bf7c57ff9262564
Avwerosuoghene_Darhare-Igben <kesuion1@gmail.com>
2026-06-29 21:03:11 +0100

    feat(roles): scaffolding of empty roles state

--- src/app/pages/main/roles/roles.component.ts
+++ src/app/pages/main/roles/roles.component.ts
@@ -1,25 +1,22 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatIconModule } from '@angular/material/icon';
 import { AnalyticsService } from '../../../core/services/analytics/analytics.service';
 import { AnalyticsEvent } from '../../../core/models/analytics-events.enum';
+import { ROLE_LIMIT } from '../../../core/models/constants/role.constants';
 
 @Component({
   selector: 'app-roles',
   standalone: true,
-  imports: [
-    CommonModule,
-    MatIconModule
-  ],
+  imports: [CommonModule],
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RolesComponent implements OnInit {
-
   private analyticsService = inject(AnalyticsService);
 
-  isLoading = false;
+  readonly roleLimit = ROLE_LIMIT;
+  readonly roleCount = 0;
 
   ngOnInit(): void {
     this.analyticsService.track(AnalyticsEvent.PAGE_VIEWED, { page: 'roles' });
